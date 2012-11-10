8977f083325f7f3e1cbabde5bcbc0efe47d6bd82
Michael Haren <mharen@gmail.com>
2012-11-09 22:46:58 -0500

    messiness related to persistence...not functional

--- devour/Board.cs
+++ devour/Board.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Globalization;
 using Microsoft.AspNet.SignalR.Hubs;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace devour
 {
@@ -13,6 +16,7 @@ namespace devour
         public static readonly int BoardWidth = 100;
         public static readonly int BoardHeight = 56;
         public static readonly int PixelCount = BoardWidth * BoardHeight;
+        public static DateTime LastSaved = DateTime.UtcNow;
 
         static Board()
         {
@@ -25,6 +29,54 @@ namespace devour
             Clients.All.Pong(id, value);
 
             return value;
+        }
+
+        public async void DumpBoard(int[] board)
+        {
+            // make sure table exists:
+
+//var checkTableCommandText = "IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND  TABLE_NAME = 'BoardHistory') BEGIN CREATE TABLE BoardHistory(Id INT IDENTITY PRIMARY KEY, Values varbinary(max) END
+            const string commandText = "INSERT INTO BoardHistory (Values) VALUES (@Values)";
+            using (var connection = new SqlConnection())
+            {
+                var command = new SqlCommand(commandText, connection);
+                var bytes = ToBytes(board);
+
+                command.Parameters.Add("@Values", SqlDbType.Binary, bytes.Length).Value = bytes;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // via http://stackoverflow.com/a/713105/29
+        private byte[] ToBytes(int[] ints)
+        {
+            // basic - same count
+            bool[] bools = Array.ConvertAll(ints, b => b%2 == 1);
+            byte[] arr1 = Array.ConvertAll(bools, b => b ? (byte)1 : (byte)0);
+
+            // pack (in this case, using the first bool as the lsb - if you want
+            // the first bool as the msb, reverse things ;-p)
+            int bytes = bools.Length / 8;
+            if ((bools.Length % 8) != 0) bytes++;
+            byte[] arr2 = new byte[bytes];
+            int bitIndex = 0, byteIndex = 0;
+            for (int i = 0; i < bools.Length; i++)
+            {
+                if (bools[i])
+                {
+                    arr2[byteIndex] |= (byte)(((byte)1) << bitIndex);
+                }
+                bitIndex++;
+                if (bitIndex == 8)
+                {
+                    bitIndex = 0;
+                    byteIndex++;
+                }
+            }
+
+            return arr2;
         }
 
         public void Reset()
